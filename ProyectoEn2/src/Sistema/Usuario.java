ed52aa60f3af7275a9ba5500b92b19c332e1134a
Ignacio Mucci <biglianiignacio@gmail.com>
2024-12-07 04:56:03 -0300

    No cambie casi nada

---
+++ ProyectoEn2/src/Sistema/Usuario.java
@@ -0,0 +1,46 @@
+package Sistema;
+
+public class Usuario {
+	private String Nombre;
+	private String Apellido;
+	private String passwr;
+	
+
+	
+	
+	public Usuario() {
+		
+		
+	}
+	
+	public String getPasswr() {
+		return passwr;
+	}
+
+
+
+
+	public void setPasswr(String passwr) {
+		this.passwr = passwr;
+	}
+	
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellido() {
+		return Apellido;
+	}
+	public void setApellido(String apellido) {
+		Apellido = apellido;
+	}
+	
+	
+	
+	
+	
+	
+}
